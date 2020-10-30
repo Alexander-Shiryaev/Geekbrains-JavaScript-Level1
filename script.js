f97abb2f799809849056663929331d40e8c62a9d
shiryaev585 <69469715+shiryaev585@users.noreply.github.com>
2020-10-30 12:25:43 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,58 +1,37 @@
-1
+function chess () {
+    let chessBoard = document.querySelector('.chessboard');
+    
+    let flag = true;
 
-let num = 1;
-while(num < 100) {
-    num++;
-    let x = 1;
-    for(let y = 2; y <= num / 2; y++) {
-        if (num % y == 0) {
-            x = 0;
+    let fig = {
+        0 : ['-263px -19px', '-484px -22px', '-372px -17px', '-150px -16px', '-40px -16px','-372px -17px', '-484px -22px','-263px -19px'],
+        1 : ['-595px -19px', '-595px -19px', '-595px -19px', '-595px -19px', '-595px -19px','-595px -19px', '-595px -19px','-595px -19px'],
+        6 : ['-595px -116px', '-595px -116px', '-595px -116px', '-595px -116px', '-595px -116px','-595px -116px', '-595px -116px','-595px -116px'],
+        7 : ['-263px -116px', '-484px -116px', '-372px -116px', '-150px -116px', '-40px -116px','-372px -116px', '-484px -116px','-263px -116px'],
+    };
+
+    for (i=0; i < 8; i++) {
+        for (j = 0; j < 8; j++) {
+            if (j == 0) {
+                flag = !flag;
+            }
+
+            let block = document.createElement('div');
+
+            if (flag) {
+                block.className = 'block black';
+            } else {
+                block.className = 'block white';
+            }
+            if (fig[i]!==undefined && fig[i][j]!==undefined){
+                block.style.backgroundImage = 'url(figures.png)';
+                block.style.backgroundPosition = fig[i][j];
+            }
+
+            chessBoard.appendChild(block);
+            flag = !flag;
         }
-    }
-    if (x == 1) {
-        console.log(num); 
     }
 }
 
-
-// 2,3
-
-// let basket = [
-//     {
-//         title: "T-short",
-//         price: 100
-//     },
-//     {
-//         title: "Jeans",
-//         price: 200
-//     },
-//     {
-//         title: "Suit",
-//         price: 300
-//     }
-// ];
-
-// function countBasketPrice(basket) {
-//     let sum = 0;
-//     for (item of basket) {
-//         sum += item.price;
-//     }
-//     return sum;
-// }
-
-// console.log(countBasketPrice(basket));
-
-
-//4*
-
-// for(let i = -1; i < 9; i++, console.log(i) ) {}
-
-// 4*
-
-// let arr = [];
-// let i = 0;
-// while (i < 20) {
-//     arr.push('x');
-//     i++;
-//     console.log(...arr);
-// }
+chess ();
